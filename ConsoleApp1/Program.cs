ce13db0533aa8f426df637677b17fee87b18f292
maximzo <zolotov_ma@ascon.ru>
2024-09-22 21:05:10 +0400

    Домашка. Задание 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Привет, как твоё имя?");
+string? name = Console.ReadLine();
+Console.WriteLine("Как твоя фамилия?");
+string? lastname = Console.ReadLine();
+Console.WriteLine("Как твое отчество?");
+string? patronymic = Console.ReadLine();
+Console.WriteLine($"Приятно познакомиться, {name} {lastname} {patronymic}");
+Console.ReadKey();
