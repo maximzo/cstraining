1f49c4d9de331ef62f98aafe5134136593cceee6
maximzo <zolotov_ma@ascon.ru>
2024-10-11 00:16:56 +0400

    Домашка 4. Задание 1

---
+++ ConsoleApp6/PrimeNum.cs
@@ -0,0 +1,35 @@
+﻿// ДЗ с математикой:
+// Написать алгоритм поиска N-го простого числа (к примеру 2, 3, 5, 7 где 2 это 1ое число 3 - второе и т.д., т.е. если N = 4 то на выходе должно быть 7)
+
+int primeNum = 0;
+int naturalNum = 1;
+Console.Write("Введите номер простого числа, которое нужно найти: ");
+if (int.TryParse(Console.ReadLine(), out var number))
+{
+    while (primeNum < number)
+    {
+        naturalNum++;
+        if (IsPrime(naturalNum))
+        {
+            primeNum++;
+        }
+    }
+    Console.WriteLine($"Простое число №{number} - {naturalNum}");
+}
+else Console.WriteLine("Введите положительное, целое число");
+Console.ReadKey();
+
+bool IsPrime(int num)
+{
+    if (num < 2)
+    {
+        return false;
+    }
+
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
